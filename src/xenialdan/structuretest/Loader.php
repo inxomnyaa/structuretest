2c10a89c3df303f9f9322e8ad6ccacca3d89fbe0
thebigsmileXD <thebigsmilexd@gmail.com>
2019-09-14 20:47:16 +0200

    Update to MWE v9.7.1 (session-cleanup branch)

--- src/xenialdan/structuretest/Loader.php
+++ src/xenialdan/structuretest/Loader.php
@@ -17,7 +17,8 @@ use pocketmine\math\Vector3;
 use pocketmine\plugin\PluginBase;
 use xenialdan\libstructure\PacketListener;
 use xenialdan\libstructure\StructureUI;
-use xenialdan\MagicWE2\API;
+use xenialdan\MagicWE2\exception\SessionException;
+use xenialdan\MagicWE2\helper\SessionHelper;
 
 class Loader extends PluginBase implements Listener
 {
@@ -32,22 +33,24 @@ class Loader extends PluginBase implements Listener
     public function onSneak(PlayerToggleSneakEvent $e)
     {
         if ($e->isSneaking()) return;
-        $session = API::getSession($e->getPlayer());
-        if (is_null($session)) return;
-        $selection = $session->getLatestSelection();
-        if (is_null($selection)) return;
         try {
-            $shape = $selection->getShape();
-        } catch (\Exception $ex) {
-            $e->getPlayer()->sendMessage($ex->getMessage());
-            return;
+            $session = SessionHelper::getUserSession($e->getPlayer());
+            if (is_null($session)) return;
+            $selection = $session->getLatestSelection();
+            if (is_null($selection)) return;
+            try {
+                $shape = $selection->getShape();
+            } catch (\Exception $ex) {
+                $e->getPlayer()->sendMessage($ex->getMessage());
+                return;
+            }
+            $aabb = $shape->getAABB();
+            $min = new Vector3($aabb->minX, $aabb->minY, $aabb->minZ);
+            $max = new Vector3($aabb->maxX, $aabb->maxY, $aabb->maxZ);
+            $name = basename(get_class($shape));
+            $menu = InvMenu::create(StructureUI::class, StructureUI::getMinV3($min, $max), StructureUI::getMaxV3($min, $max), $name);
+            $menu->send($e->getPlayer());
+        } catch (SessionException $e) {
         }
-        $aabb = $shape->getAABB();
-        $min = new Vector3($aabb->minX, $aabb->minY, $aabb->minZ);
-        $max = new Vector3($aabb->maxX, $aabb->maxY, $aabb->maxZ);
-        $name = basename(get_class($shape));
-        $menu = InvMenu::create(StructureUI::class, StructureUI::getMinV3($min, $max), StructureUI::getMaxV3($min, $max), $name);
-        var_dump($menu->getInventory());
-        $menu->send($e->getPlayer());
     }
 }
